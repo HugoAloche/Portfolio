ea9867880d32d1f796cfda5186e1d9dc7b681c7b
= <=>
2022-06-08 21:08:57 +0200

    feat: modif pages

--- pages/index.vue
+++ pages/index.vue
@@ -215,6 +215,7 @@ export default {
       await this.$recaptcha.execute('login')
       this.GestionPromesse(
         this.$mail.send({
+          header: 'Access-Control-Allow-Origin: portfolio-hugo.herokuapp.com',
           from: 'haloche035@gmail.com',
           replyTo: this.email,
           subject: this.objet,
